aaaa3ca4f704ad363982c707decf597cbf2be04d
Timothée Benchergui <79049541+timo0135@users.noreply.github.com>
2024-01-29 11:24:16 +0100

    Update Configuration.php changement du nom de la page web td2

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -36,7 +36,7 @@ class Configuration
      */
     private static $_defaults = array(
         'main' => array(
-            'name'                     => 'PrivateBin',
+            'name'                     => 'CharleBin',
             'basepath'                 => '',
             'discussion'               => true,
             'opendiscussion'           => false,
