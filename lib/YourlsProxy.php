58e2e64e0e17081705a638c9c1cfd7c10d4f160c
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2024-02-05 10:50:24 +0100

    fixed two errors/warnings

--- lib/YourlsProxy.php
+++ lib/YourlsProxy.php
@@ -2,9 +2,9 @@
 /**
  * PrivateBin
  *
- * a zero-knowledge paste bin
+ * A zero-knowledge paste bin
  *
- * @link      https://github.com/PrivateBin/PrivateBin
+ * @copyright      https://github.com/PrivateBin/PrivateBin
  * @copyright 2012 Sébastien SAUVAGE (sebsauvage.net)
  * @license   https://www.opensource.org/licenses/zlib-license.php The zlib/libpng License
  * @version   1.5.1
